8af8ae7592f006d2a2658f4efe2e37842b61059d
chekst <israel.gavriel@gmail.com>
2018-04-17 16:22:55 +0800

    refactoring

--- joystick.js
+++ joystick.js
@@ -6,53 +6,13 @@ var JoystickManager = {
     point: null,
     diameter: 100,
     pointDiameter: 10,
-    distance: function(coords) {
-        var xPow2 = Math.pow((JoystickManager.startPosition[0] - coords[0]), 2)
-        var yPow2 = Math.pow((JoystickManager.startPosition[1] - coords[1]), 2)
-        return Math.sqrt(xPow2 + yPow2)
-    },
-    clearJoystick: function() {
-        JoystickManager.circle.style.display = "none";
-    },
-    setJoystick: function() {
-        var left = JoystickManager.startPosition[0] - JoystickManager.container.offsetLeft - JoystickManager.diameter/2;
-        var top = JoystickManager.startPosition[1] - JoystickManager.container.offsetTop - JoystickManager.diameter/2;
-        JoystickManager.circle.style.top = top + "px";
-        JoystickManager.circle.style.left = left + "px";
-        JoystickManager.circle.style.display = "block";
-
-        JoystickManager.movePoint(JoystickManager.startPosition)
-    },
-
-    movePoint: function(coords) {
-        if (JoystickManager.started) {
-            var dist = JoystickManager.distance(coords)
-            var left = coords[0] - JoystickManager.startPosition[0];
-            var top = coords[1] - JoystickManager.startPosition[1];
-            if (dist > JoystickManager.diameter/2) {
-                left = (left * JoystickManager.diameter) / (dist * 2)
-                top = (top * JoystickManager.diameter) / (dist * 2)
-            }
-            JoystickManager.point.style.top = JoystickManager.diameter/2 - JoystickManager.pointDiameter/2 + top + "px";
-            JoystickManager.point.style.left = JoystickManager.diameter/2 - JoystickManager.pointDiameter/2 + left + "px";    
-        }
-    },
-
-
-    startMove: function(coords) {
-        JoystickManager.started = true;
-        JoystickManager.startPosition = coords;
-        JoystickManager.setJoystick(coords)
-    },
-    stopMove: function() {
-        JoystickManager.started = false;
-        JoystickManager.startPosition = [null, null];
-        JoystickManager.clearJoystick();
-    },
     init: function() {
         JoystickManager.container = document.getElementById("joystick-container");
         JoystickManager.circle = document.getElementById("circle");
         JoystickManager.point = document.getElementById("point");
+        JoystickManager.initEvents();
+    },
+    initEvents: function() {
         JoystickManager.container.onmousedown = function(e){
             e.preventDefault();
             var coords = [e.clientX, e.clientY];
@@ -89,5 +49,45 @@ var JoystickManager = {
             e.preventDefault();
             JoystickManager.stopMove();
         };
+    },
+    distance: function(coords) {
+        var xPow2 = Math.pow((JoystickManager.startPosition[0] - coords[0]), 2)
+        var yPow2 = Math.pow((JoystickManager.startPosition[1] - coords[1]), 2)
+        return Math.sqrt(xPow2 + yPow2)
+    },
+    clearJoystick: function() {
+        JoystickManager.circle.style.display = "none";
+    },
+    setJoystick: function() {
+        var left = JoystickManager.startPosition[0] - JoystickManager.container.offsetLeft - JoystickManager.diameter/2;
+        var top = JoystickManager.startPosition[1] - JoystickManager.container.offsetTop - JoystickManager.diameter/2;
+        JoystickManager.circle.style.top = top + "px";
+        JoystickManager.circle.style.left = left + "px";
+        JoystickManager.circle.style.display = "block";
+
+        JoystickManager.movePoint(JoystickManager.startPosition)
+    },
+    movePoint: function(coords) {
+        if (JoystickManager.started) {
+            var dist = JoystickManager.distance(coords)
+            var left = coords[0] - JoystickManager.startPosition[0];
+            var top = coords[1] - JoystickManager.startPosition[1];
+            if (dist > JoystickManager.diameter/2) {
+                left = (left * JoystickManager.diameter) / (dist * 2)
+                top = (top * JoystickManager.diameter) / (dist * 2)
+            }
+            JoystickManager.point.style.top = JoystickManager.diameter/2 - JoystickManager.pointDiameter/2 + top + "px";
+            JoystickManager.point.style.left = JoystickManager.diameter/2 - JoystickManager.pointDiameter/2 + left + "px";    
+        }
+    },
+    startMove: function(coords) {
+        JoystickManager.started = true;
+        JoystickManager.startPosition = coords;
+        JoystickManager.setJoystick(coords)
+    },
+    stopMove: function() {
+        JoystickManager.started = false;
+        JoystickManager.startPosition = [null, null];
+        JoystickManager.clearJoystick();
     },
 };
